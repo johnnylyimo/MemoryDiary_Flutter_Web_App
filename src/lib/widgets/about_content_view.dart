d30d7914580964f8e6c2d6b3951dabb206f0d369
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:32:25 +0300

    Created StatelessWidget named AboutContentView

---
+++ src/lib/widgets/about_content_view.dart
@@ -0,0 +1,8 @@
+class AboutContentView extends StatelessWidget {
+  const AboutContentView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
