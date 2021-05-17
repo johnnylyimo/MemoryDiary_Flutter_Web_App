9b4b822da33922ef900387cc923e14e2efb6a07e
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:52:37 +0300

    Created CustomTabBar Constructor

--- src/lib/widgets/custom_tab_bar.dart
+++ src/lib/widgets/custom_tab_bar.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class CustomTabBar extends StatelessWidget {
   final TabController controller;
   final List<Widget> tabs;
+
+  const CustomTabBar({required this.controller, required this.tabs});
+
   @override
   Widget build(BuildContext context) {
     return Container();
