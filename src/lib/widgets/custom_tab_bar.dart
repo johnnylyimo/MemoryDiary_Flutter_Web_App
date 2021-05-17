8f1bd47b80ad5b3d3c31dabb38eccee8455cc77d
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:58:20 +0300

    Assigned Container child to TabBar widget

--- src/lib/widgets/custom_tab_bar.dart
+++ src/lib/widgets/custom_tab_bar.dart
@@ -17,6 +17,7 @@ class CustomTabBar extends StatelessWidget {
     // if is between 1400 and 1100 is 0.3 and else small than that 0.4
     return Container(
       width: screenWidth * tabBarScaling,
+      child: TabBar(),
     );
   }
 }
