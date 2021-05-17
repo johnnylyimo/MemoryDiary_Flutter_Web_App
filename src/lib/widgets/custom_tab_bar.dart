a6da860765422a79796a6fb262466d36f024f879
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:57:15 +0300

    Assigned Container width

--- src/lib/widgets/custom_tab_bar.dart
+++ src/lib/widgets/custom_tab_bar.dart
@@ -15,6 +15,8 @@ class CustomTabBar extends StatelessWidget {
             ? 0.3
             : 0.4;
     // if is between 1400 and 1100 is 0.3 and else small than that 0.4
-    return Container();
+    return Container(
+      width: screenWidth * tabBarScaling,
+    );
   }
 }
