3a4f8787fbff2ec728687c8b8db5de328e8f76f0
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:56:32 +0300

    Initialized tabBarScaling variable

--- src/lib/widgets/custom_tab_bar.dart
+++ src/lib/widgets/custom_tab_bar.dart
@@ -9,6 +9,12 @@ class CustomTabBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double tabBarScaling = screenWidth > 1400
+        ? 0.21
+        : screenWidth > 1100
+            ? 0.3
+            : 0.4;
+    // if is between 1400 and 1100 is 0.3 and else small than that 0.4
     return Container();
   }
 }
