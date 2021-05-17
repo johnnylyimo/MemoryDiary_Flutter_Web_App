ebfe5d905515bf505497dff9f3927a88e7ca0a89
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 16:28:55 +0300

    Set purple color to primaryColor

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.purple,
+          primaryColor: Colors.purple,
         ),
         home: NavigationBar());
   }
