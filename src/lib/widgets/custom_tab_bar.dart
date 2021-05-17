4cea7579696bf53db518ddcad0ed35e801f929a0
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:51:16 +0300

    Defined List variable tabs

--- src/lib/widgets/custom_tab_bar.dart
+++ src/lib/widgets/custom_tab_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustomTabBar extends StatelessWidget {
   final TabController controller;
+  final List<Widget> tabs;
   @override
   Widget build(BuildContext context) {
     return Container();
