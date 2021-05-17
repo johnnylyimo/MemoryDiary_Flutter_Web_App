181d2dee5e5ac69fd467ea221a85a0df3fa1b599
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:50:45 +0300

    Created TabController instance

--- src/lib/widgets/custom_tab_bar.dart
+++ src/lib/widgets/custom_tab_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomTabBar extends StatelessWidget {
+  final TabController controller;
   @override
   Widget build(BuildContext context) {
     return Container();
