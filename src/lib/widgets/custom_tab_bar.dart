601251d65cd1fbad90e1632923f64d0528d414cf
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:59:00 +0300

    Assigned TabBar controller

--- src/lib/widgets/custom_tab_bar.dart
+++ src/lib/widgets/custom_tab_bar.dart
@@ -17,7 +17,9 @@ class CustomTabBar extends StatelessWidget {
     // if is between 1400 and 1100 is 0.3 and else small than that 0.4
     return Container(
       width: screenWidth * tabBarScaling,
-      child: TabBar(),
+      child: TabBar(
+        controller: controller,
+      ),
     );
   }
 }
