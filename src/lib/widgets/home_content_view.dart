a729732b118d97990bfc4f1417aee3596c6c8d0f
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:30:15 +0300

    Created StatefulWidget named HomeContentView

---
+++ src/lib/widgets/home_content_view.dart
@@ -0,0 +1,13 @@
+class HomeContentView extends StatefulWidget {
+  const HomeContentView({Key key}) : super(key: key);
+
+  @override
+  _HomeContentViewState createState() => _HomeContentViewState();
+}
+
+class _HomeContentViewState extends State<HomeContentView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
