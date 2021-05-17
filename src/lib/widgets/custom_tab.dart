bb32347d0711b49dc70c4be8f889276b327a7480
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-17 04:14:52 +0300

    Declared class property of type String

--- src/lib/widgets/custom_tab.dart
+++ src/lib/widgets/custom_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomTab extends StatelessWidget {
+  final String title;
   @override
   Widget build(BuildContext context) {
     return Container();
