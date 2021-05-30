d3cbc07125bf77c9dc76eaf382dd5b51c0d7df2b
JohnnyLyimo <johnnylyimo@gmail.com>
2021-05-30 20:44:53 +0300

    Defined TextSpan style fontWeight to bold

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -40,6 +40,7 @@ class _HomeContentViewState extends State<HomeContentView> {
               text: TextSpan(
                   style: TextStyle(
                 fontSize: 16.0,
+                fontWeight: FontWeight.bold,
               )),
             ),
           ),
