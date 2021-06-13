713b8286029de0075837ed92084c5326efc61ceb
JohnnyLyimo <johnnylyimo@gmail.com>
2021-06-14 02:18:35 +0300

    Defined Text value

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -105,7 +105,7 @@ class _HomeContentViewState extends State<HomeContentView> {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(),
+              Text('Highlights'),
             ],
           )
         ],
