ca71c8bf0ee632b7987640920c59f900d88556a6
JohnnyLyimo <johnnylyimo@gmail.com>
2021-06-14 02:14:35 +0300

    Defined TextSpan text property

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -89,12 +89,16 @@ class _HomeContentViewState extends State<HomeContentView> {
             child: RichText(
               textAlign: TextAlign.center,
               text: TextSpan(
-                text: 'WORD OF THE DAY\n',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+                  text: 'WORD OF THE DAY\n',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Today will be better than yesterday',
+                    ),
+                  ]),
             ),
           ),
         ],
