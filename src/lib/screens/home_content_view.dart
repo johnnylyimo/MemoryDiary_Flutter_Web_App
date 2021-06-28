287188b2d0ecf8d3d496a12e2079288de0538174
JohnnyLyimo <johnnylyimo@gmail.com>
2021-06-28 19:23:28 +0300

    Added Container to the Column children

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -68,6 +68,7 @@ class _HomeContentViewState extends State<HomeContentView> {
                 SizedBox(
                   height: 20.0,
                 ),
+                Container(),
               ],
             ),
           ),
