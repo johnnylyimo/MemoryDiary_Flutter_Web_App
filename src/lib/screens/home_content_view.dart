8294876b0e23e8c2e11458c39711614a84716a6d
JohnnyLyimo <johnnylyimo@gmail.com>
2021-06-28 19:18:15 +0300

    Wrapped RichText with Column widget

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -48,19 +48,21 @@ class _HomeContentViewState extends State<HomeContentView> {
             width: 360.0,
             color: Colors.purple.shade200,
             padding: EdgeInsets.all(5.0),
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: 'WORD OF THE DAY\n',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                children: [
-                  TextSpan(
-                    text: 'Today will be better than yesterday',
+            child: Column(
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'WORD OF THE DAY\n',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
+                  children: [
+                    TextSpan(
+                      text: 'Today will be better than yesterday',
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
