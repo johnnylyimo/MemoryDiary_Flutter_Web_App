812ccfca2e16bd55d3bc1d63190666a62f6d39e1
JohnnyLyimo <johnnylyimo@gmail.com>
2021-07-18 17:33:17 +0300

    Assigned Container child to Form widget

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -223,6 +223,7 @@ class _HomeContentViewState extends State<HomeContentView> {
             width: MediaQuery.of(context).size.width * 0.5,
             height: MediaQuery.of(context).size.height * 0.25,
             padding: EdgeInsets.all(20.0),
+            child: Form(),
           ),
         );
       },
