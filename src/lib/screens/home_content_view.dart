aeea1182b5bebf43d69d64d264c618e8e982a8ca
JohnnyLyimo <johnnylyimo@gmail.com>
2021-07-25 18:45:49 +0300

    Assigned true value to mobile view  EditDialog method

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -413,9 +413,9 @@ class _HomeContentViewState extends State<HomeContentView> {
                                   trailing: IconButton(
                                     icon: Icon(Icons.edit),
                                     onPressed: () => EditDialog(
-                                      memories.values.elementAt(index),
-                                      memories.keys.elementAt(index),
-                                    ),
+                                        memories.values.elementAt(index),
+                                        memories.keys.elementAt(index),
+                                        true),
                                   ),
                                 ),
                               );
