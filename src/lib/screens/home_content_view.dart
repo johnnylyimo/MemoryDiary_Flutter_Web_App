e654730a3246d2df599278110e6b631ae39c5d5a
JohnnyLyimo <johnnylyimo@gmail.com>
2021-08-08 23:22:07 +0300

    Defined Text widget overflow property

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -244,6 +244,7 @@ class _HomeContentViewState extends State<HomeContentView> {
                 "MolysTechie is tech startup, specialized on developing Mobile Apps, Web App and Wep APIs. It's senior Developer is John L. Lyimo, the guy who developed this Website that is powered by Flutter and hosted on GitHub pages.",
                 textAlign: TextAlign.center,
                 maxLines: 8,
+                overflow: TextOverflow.ellipsis,
               )
             ],
           ),
