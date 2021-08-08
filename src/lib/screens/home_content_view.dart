ed79895756969ad219849fe6a427b50c2f8bc3a6
JohnnyLyimo <johnnylyimo@gmail.com>
2021-08-08 23:45:47 +0300

    Added TextButton widget to Column  children

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -295,6 +295,7 @@ class _HomeContentViewState extends State<HomeContentView> {
                       ),
                       onPressed: () {},
                     ),
+                    TextButton(),
                   ],
                 ),
               )
