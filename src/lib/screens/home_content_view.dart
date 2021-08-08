c41e5b23be5f28b74c0cfe36f1c57ed34041e5ef
JohnnyLyimo <johnnylyimo@gmail.com>
2021-08-08 23:35:17 +0300

    Defined SizedBox height property

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -268,7 +268,9 @@ class _HomeContentViewState extends State<HomeContentView> {
                         letterSpacing: 0.5,
                       ),
                     ),
-                    SizedBox(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
                   ],
                 ),
               )
