1c90d4505688b50d28e7f4c2845e9ee3455551c7
JohnnyLyimo <johnnylyimo@gmail.com>
2021-08-15 16:16:09 +0300

    Added Text widget to Column children

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -358,7 +358,8 @@ class _HomeContentViewState extends State<HomeContentView> {
                     ),
                     SizedBox(
                       height: 10.0,
-                    )
+                    ),
+                    Text(),
                   ],
                 ),
               ),
