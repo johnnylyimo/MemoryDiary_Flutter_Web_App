b28cbf7e804184c1ba6ac81f4beaed405a293fec
JohnnyLyimo <johnnylyimo@gmail.com>
2021-08-15 18:14:14 +0300

    Invoked _launchURL method to onPressed method

--- src/lib/screens/home_content_view.dart
+++ src/lib/screens/home_content_view.dart
@@ -312,7 +312,7 @@ class _HomeContentViewState extends State<HomeContentView> {
                           letterSpacing: 0.5,
                         ),
                       ),
-                      onPressed: () {},
+                      onPressed: () => _launchURL(),
                     ),
                   ],
                 ),
